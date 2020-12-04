51832718a2517885b3aac5626f0ae5754fa026c2
Tzesh <mrtzesh@gmail.com>
2020-12-04 21:38:37 +0300

    fixed OS dependent input coding

--- src/main/java/Interface.java
+++ src/main/java/Interface.java
@@ -1,9 +1,7 @@
 import utils.LinkedList;
 
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
@@ -11,7 +9,7 @@ public class Interface {
     public static LinkedList<Person> persons = new LinkedList<>();
     public static LinkedList<Song> songs = new LinkedList<>();
 
-    public static void main(String[] args) throws UnsupportedEncodingException {
+    public static void main(String[] args) {
         try {
             System.setOut(new PrintStream(System.out, true, "UTF-8")); // to avoid platform (OS) dependent outputs like Uğur -> U??ur
         } catch (UnsupportedEncodingException e) {
@@ -27,10 +25,9 @@ public class Interface {
                 "\nM: List all the songs that liked by anyone." +
                 "\nR: Recommends the most popular 3 different songs.");
         System.out.print("> ");  // that '>' symbol indicates the program awaiting a response from user
-        Scanner scanner = null;
-        scanner = new Scanner(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+        Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8);
         while (true) { // while loop is required to always get inputs from user
-            if (!CommandManager.handle(new String(scanner.next().getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8)))  // we'll use CommandManager class which helps out to simply and organize our code
+            if (!CommandManager.handle(scanner.nextLine()))  // we'll use CommandManager class which helps out to simply and organize our code
                 System.out.println("Please supply an argument."); // if given input is not suitable we must get a new input
             System.out.print("> "); // that '>' symbol indicates the program awaiting a response from user
         }
